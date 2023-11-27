73809d538d5f67a7de2cfd4a3271bc6327086680
Antonio Cheong <acheong@student.dalat.org>
2023-11-27 04:53:11 +0000

    modules are ugly

--- example/test_wcf.js
+++ example/test_wcf.js
@@ -1,8 +1,2 @@
-(async () => {
-  const module = await import(
-    "https://cdn.jsdelivr.net/gh/g-utils/WebComponentFactory/wcf.js"
-  );
-  const { register_component } = module;
-  register_component("test-wcf", "test_wcf.html", false);
-  register_component("test-wcf-shadow", "test_wcf_shadow.html", true);
-})();
+register_component("test-wcf", "test_wcf.html", false);
+register_component("test-wcf-shadow", "test_wcf_shadow.html", true);
